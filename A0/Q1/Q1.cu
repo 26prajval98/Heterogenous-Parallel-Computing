dfd1fe368f284f15e13fda622808cff2084ea434
SUMUKHA-PK <sumukhapk46@gmail.com>
2018-07-24 22:32:56 +0530

    Frickin almost done

--- A0/Q1/Q1.cu
+++ A0/Q1/Q1.cu
@@ -1,18 +1,3 @@
-/*
-* Copyright 1993-2015 NVIDIA Corporation.  All rights reserved.
-*
-* Please refer to the NVIDIA end user license agreement (EULA) associated
-* with this source code for terms and conditions that govern your use of
-* this software. Any use, reproduction, disclosure, or distribution of
-* this software and related documentation outside the terms of the EULA
-* is strictly prohibited.
-*
-*/
-/* This sample queries the properties of the CUDA devices present in the system
-* via CUDA Runtime API. */
-
-// std::system includes
-
 #include <cuda_runtime.h>
 #include <helper_cuda.h>
 

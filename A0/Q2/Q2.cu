9982a39c7f38dc0ee8fc0c4fe55fcb633c2c80a7
Sumukha Pk <sumukhapk46@gmail.com>
2018-07-25 07:20:35 +0530

    Update Q2.cu

--- A0/Q2/Q2.cu
+++ A0/Q2/Q2.cu
@@ -4,7 +4,7 @@
 #include<stdio.h>
 #include<cuda.h>
 
-__global__ void add_vec(float *d_a, int n){		                         //7.CUDA Kernel that computes sum
+__global__ void add_vec(float *d_a, int n){		                              //7.CUDA Kernel that computes sum
 	int i = threadIdx.x;
 	if((n-i-1)!=i)	{
 		d_a[i]+=d_a[n-i-1];
@@ -20,14 +20,14 @@ int main(){
 	if(deviceCount > 0){
 
 		cudaDeviceProp devProp;
-		cudaGetDeviceProperties(&devProp, 0);							// Use default GPU
+		cudaGetDeviceProperties(&devProp, 0);		                     // Use default GPU
 		
 		#define MAX_THREAD devProp.maxThreadsPerBlock
 
 		printf("Enter the size of the array: ");
 		scanf("%d",&n);
 		
-		if(n < MAX_THREAD){											   // Do not sum if array size is greater than thread-size per block 
+		if(n < MAX_THREAD){			                             // Do not sum if array size is greater than thread-size per block 
 
 			float *h_a;
 			h_a = (float *)malloc(sizeof(float)*n);
@@ -43,14 +43,14 @@ int main(){
 
 			for(i=n;i>1;i= i%2?i/2 + 1:i/2){
 				threads_used = i/2;                                 
-				add_vec<<<Block_size,threads_used>>>(d_a,i);         	 //4.Invoke kernel
+				add_vec<<<Block_size,threads_used>>>(d_a,i);         //4.Invoke kernel
 			}
 
 			float z;
 			
 			cudaMemcpy(&z,&d_a[0],sizeof(float),cudaMemcpyDeviceToHost); //5.Copy results from device to host 
 			cudaFree(d_a);                                               //6.Free device memory
-			free(h_a);                         			     			 // free host memory
+			free(h_a);                         			     // free host memory
 			printf("The sum is : %f \n",z);    
 
 			return 0;

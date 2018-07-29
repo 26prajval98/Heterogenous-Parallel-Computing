d8e08f16724b09c076ade1602cee0a495955966b
SUMUKHA-PK <sumukhapk46@gmail.com>
2018-07-29 18:06:44 +0530

    Almost done

--- A1/Q2.cu
+++ A1/Q2.cu
@@ -1,48 +1,60 @@
 #include<stdio.h>
 #include<cuda.h>
 #include<math.h>
-
+__global__ void Matrix_Add(float *d_m, float *d_n, float *d_s, long long int a, long long int b)
+{
+	long long int i = blockIdx.y*blockDim.y + threadIdx.y;
+	long long int j = blockIdx.x*blockDim.x + threadIdx.x;
+	
+	if(i<a)
+	{
+		if(j<b)
+			*(d_s + i*a + j) = *(d_m + i*a +j) + *(d_n + i*a + j);
+	}
+	
+}
 int main()
 {
-	long long int a,b,c,i,j;
+	long long int a,b,i,j;
 	printf("Enter the dimensions of the 2 matrices: ");
-	scanf("%lld %lld %lld",&a,&b,&c);                     //Matrices are aXb and bXc
+	scanf("%lld %lld",&a,&b);                             //Matrices are aXb
 	float *h_m = (float *)malloc(a*b*sizeof(float));
-	float *h_n = (float *)malloc(b*c*sizeof(float));
-	float *h_s = (float *)malloc(a*c*sizeof(float));      //aXb * bXc = aXc
+	float *h_n = (float *)malloc(b*a*sizeof(float));
+	float *h_s = (float *)malloc(a*b*sizeof(float));
 	srand((unsigned int)time(NULL));                      //Seeding the random function
 	for(i=0;i<a;i++)
 	{
 		for(j=0;j<b;j++)
 			*(h_m + i*a +j) = ((float)rand());
 	}
-	for(i=0;i<b;i++)
+	for(i=0;i<a;i++)
 	{
-		for(j=0;j<c;j++)
+		for(j=0;j<b;j++)
 			*(h_n + i*b +j) = ((float)rand());
 	}
+	
 	dim3 DimBlock(16,16,1);
 	dim3 DimGrid(ceil(n/16),ceil(n/16),1);
 
 	cudaMalloc((void**)&d_m,a*b*sizeof(float));
-	cudaMalloc((void**)&d_n,b*c*sizeof(float));
-	cudaMallic((void**)&d_s,a*c*sizeof(float));
+	cudaMalloc((void**)&d_n,b*a*sizeof(float));
+	cudaMalloc((void**)&d_s,a*b*sizeof(float));
 
 	cudaMemcpy(d_m,h_m,a*b*sizeof(float),cudaMemcpyHostToDevice);
-	cudaMemcpy(d_n,h_n,b*c*sizeof(float),cudaMemcpyHostToDevice);
-	cudaMemcpy(d_s,h_s,a*c*sizeof(float),cudaMemcpyHostToDevice);
+	cudaMemcpy(d_n,h_n,b*a*sizeof(float),cudaMemcpyHostToDevice);
+	cudaMemcpy(d_s,h_s,a*b*sizeof(float),cudaMemcpyHostToDevice);
 	 
-	Matrix_Mul<<<DimGrid,DimBlock>>(*d_m,*d_n,*d_s,a,b,c);
+	Matrix_Add<<<DimGrid,DimBlock>>(*d_m,*d_n,*d_s,a,b);
 	
 	cudaMemcpy(h_m,d_m,a*b*sizeof(float),cudaMemcpyDeviceToHost);
-        cudaMemcpy(h_n,d_n,b*c*sizeof(float),cudaMemcpyDeviceToHost);
-        cudaMemcpy(h_s,d_s,a*c*sizeof(float),cudaMemcpyDeviceToHost);
+        cudaMemcpy(h_n,d_n,b*a*sizeof(float),cudaMemcpyDeviceToHost);
+        cudaMemcpy(h_s,d_s,a*b*sizeof(float),cudaMemcpyDeviceToHost);
 	
 	cudaFree(d_m); cudaFree(d_n); cudaFree(d_s); 
 	
-	for(i=0;i<a;i++)	
+	for(i=0;i<a;i++)
 	{
-		for(j=0;j<c;j++)
+		for(j=0;j<b;j++)
 			printf("%f ",*(h_s + i*a + j));
 		printf("\n");
 	}

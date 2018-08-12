f0f1ae008eeaa37094227c87f57df8efb3ffbc67
26prajval98 <26prajval98@gmail.com>
2018-08-12 14:46:55 +0530

    Q2 done

--- A3/Q2/Q2.cu
+++ A3/Q2/Q2.cu
@@ -4,17 +4,25 @@
 #include<stdio.h>
 #include<cuda.h>
 #include<math.h>
+
+# define SIZE 16 
+ 
 __global__ void Matrix_Mul(long long int *d_m, long long int *d_n, long long int *d_p, long long int a, long long int b,long long int c)
 {
-	long long int i = blockIdx.y*blockDim.y + threadIdx.y;
-	long long int j = blockIdx.x*blockDim.x + threadIdx.x;
+	long long int oi = blockIdx.y*blockDim.y + threadIdx.y;
+	long long int oj = blockIdx.x*blockDim.x + threadIdx.x;
 	long long int temp = 0;
 
-	for(long long int k=0; k<b; k++)
+	long long int i = min(oi, a-1);
+	long long int j = min(oj, c-1);
+
+	for(long long int k=0; k<b; k++){
 		temp += d_m[i*b + k] * d_n[b*k + j]; 
+	}
 
-	d_p[i*b + j]  =  temp;
-
+	d_p[i*c + j]  =  temp;
+	__syncthreads();
+	
 }
 int main()
 {
@@ -24,20 +32,23 @@ int main()
 	long long int *h_m = (long long int *)malloc(a*b*sizeof(long long int));
 	long long int *h_n = (long long int *)malloc(b*c*sizeof(long long int));
 	long long int *h_p = (long long int *)malloc(a*c*sizeof(long long int));
+
 	srand((unsigned int)time(NULL));                                                 //Seeding the random function
+
 	for(i=0;i<a;i++)
 	{
 		for(j=0;j<b;j++)
-			*(h_m + i*b +j) = ((long long int)rand());
+			h_m[i*b + j] = ((long long int)rand());
 	}
+
 	for(i=0;i<b;i++)
 	{
 		for(j=0;j<c;j++)
-			*(h_n + i*c +j) = ((long long int)rand());
+			h_n[i*c + j] = ((long long int)rand());
 	}
 	
-	// dim3 DimGrid(ceil(a),ceil(c),1);
-	dim3 DimBlock(a,c,1);
+	dim3 DimGrid((ceil(a)/SIZE),(ceil(c)/SIZE),1);
+	dim3 DimBlock(SIZE,SIZE,1);
 	
 	long long int *d_m, *d_n, *d_p;
 	cudaMalloc((long long int**)&d_m,a*b*sizeof(long long int));
@@ -49,29 +60,31 @@ int main()
 	cudaMemcpy(d_p,h_p,a*c*sizeof(long long int),cudaMemcpyHostToDevice);
 	 
 	Matrix_Mul<<<1,DimBlock>>>(d_m,d_n,d_p,a,b,c);
-	
+		
 	cudaMemcpy(h_m,d_m,a*b*sizeof(long long int),cudaMemcpyDeviceToHost);
     cudaMemcpy(h_n,d_n,b*c*sizeof(long long int),cudaMemcpyDeviceToHost);
     cudaMemcpy(h_p,d_p,a*c*sizeof(long long int),cudaMemcpyDeviceToHost);
 	
 	for(i=0;i<a;i++)
     {
-        for(j=0;j<c;j++)
-            printf("%lld ",*(h_p + i*c + j));
+        for(j=0;j<c;j++){
+			printf("%lld ", h_p[i*c + j]);
+		}
         printf("\n");
     }
     printf("\n");
     for(i=0;i<a;i++)
     {
         for(j=0;j<b;j++)
-            printf("%lld ",*(h_m + i*b + j));
+            printf("%lld ", h_m[i*b + j] );
         printf("\n");
     }
-    printf("\n");
+	printf("\n");
+	
 	for(i=0;i<b;i++)
 	{
 		for(j=0;j<c;j++)
-			printf("%lld ",*(h_n + i*c + j));
+			printf("%lld ", h_n[i*c + j]);
 		printf("\n");
 	}
 

a25eb16a6baf67e51c1a135f2a5265039acafc4d
Sumukha Pk <sumukhapk46@gmail.com>
2018-08-22 02:06:07 +0530

    Testing

--- A4/4.Convolution/template.cu
+++ A4/4.Convolution/template.cu
@@ -1,4 +1,5 @@
-#include <wb.h>
+#include "wb.h"
+
 
 #define wbCheck(stmt)                                                     \
   do {                                                                    \
@@ -11,11 +12,35 @@
 
 #define Mask_width 5
 #define Mask_radius Mask_width / 2
-#define TILE_WIDTH 16
-#define w (TILE_WIDTH + Mask_width - 1)
+#define SIZE 16
+#define w (SIZE + Mask_width - 1)
 #define clamp(x) (min(max((x), 0.0), 1.0))
 
-//@@ INSERT CODE HERE
+__global__ void convolution(float* deviceInputImageData,float* deviceMaskData,float* deviceOutputImageData,int imageChannels,int imageWidth,int imageHeight)
+{
+  int i = blockDim.x * blockIdx.x + threadIdx.x;
+  int j = blockDim.y * blockIdx.y + threadIdx.y; 
+  int k ;
+  for(k=0;k<imageChannels;k++)
+  {
+    float accum  = 0;
+    for(int y = -Mask_radius; y< Mask_radius ; y++)
+    {
+      for(int x = -Mask_radius; x< Mask_radius ; x++)
+      {
+        int xOff = j+x;
+        int yOff = i+y;
+        if(xOff >=0 && xOff< imageWidth && yOff >=0 && yOff <imageHeight)
+        {
+          int temp = deviceInputImageData[(yOff*imageWidth + xOff)*imageChannels +  k];
+          int temp1 = deviceMaskData[(y+Mask_radius)*Mask_width+x+Mask_radius];
+          accum += temp*temp1;
+        }
+      }
+    }
+    deviceOutputImageData[(i*imageWidth+j)*imageChannels + k] = clamp(accum);
+  }
+}
 
 int main(int argc, char *argv[]) {
   wbArg_t arg;
@@ -58,25 +83,41 @@ int main(int argc, char *argv[]) {
   wbTime_start(GPU, "Doing GPU Computation (memory + compute)");
 
   wbTime_start(GPU, "Doing GPU memory allocation");
-  //@@ INSERT CODE HERE
+  
+  cudaMalloc((void **) deviceInputImageData,imageWidth * imageHeight * imageChannels * sizeof(float));                    //1.Allocate device memory
+  cudaMalloc((void **) deviceMaskData,sizeof(hostMaskData));
+  cudaMalloc((void **) deviceOutputImageData,sizeof(hostOutputImageData));
+  // cudaMalloc((void **) imageChannels,sizeof(int));
+  // cudaMalloc((void **) imageHeight, sizeof(int));
+  // cudaMalloc((void **) imageWidth,sizeof(int));
+
   wbTime_stop(GPU, "Doing GPU memory allocation");
 
   wbTime_start(Copy, "Copying data to the GPU");
-  //@@ INSERT CODE HERE
+
+  cudaMemcpy(deviceInputImageData,hostInputImageData,imageWidth * imageHeight * imageChannels * sizeof(float),cudaMemcpyHostToDevice);   // 2.Copt host to device
+  cudaMemcpy(deviceMaskData,hostMaskData,sizeof(hostMaskData),cudaMemcpyHostToDevice);
+
+  // cudaMemcpy(,cudaMemcpyHostToDevice);
+  // cudaMemcpy(,cudaMemcpyHostToDevice);
+  // cudaMemcpy(,cudaMemcpyHostToDevice);
+
   wbTime_stop(Copy, "Copying data to the GPU");
 
   wbTime_start(Compute, "Doing the computation on the GPU");
-  //@@ INSERT CODE HERE
-  convolution<<<dimGrid, dimBlock>>>(deviceInputImageData, deviceMaskData,
+  
+  dim3 dimBlock(SIZE);                                                                  //3.inititalise thread block and grid dimensions
+  dim3 dimGrid(ceil(imageHeight/SIZE),ceil(imageWidth/SIZE),1);
+
+  convolution<<<dimGrid, dimBlock>>>(deviceInputImageData, deviceMaskData,              //4.Invoke CUDA kernel
                                      deviceOutputImageData, imageChannels,
                                      imageWidth, imageHeight);
   wbTime_stop(Compute, "Doing the computation on the GPU");
 
   wbTime_start(Copy, "Copying data from the GPU");
-  //@@ INSERT CODE HERE
-  cudaMemcpy(hostOutputImageData, deviceOutputImageData,
-             imageWidth * imageHeight * imageChannels * sizeof(float),
-             cudaMemcpyDeviceToHost);
+ 
+  cudaMemcpy(hostOutputImageData, deviceOutputImageData,imageWidth * imageHeight * imageChannels * sizeof(float),cudaMemcpyDeviceToHost); //5. Copy results
+        
   wbTime_stop(Copy, "Copying data from the GPU");
 
   wbTime_stop(GPU, "Doing GPU Computation (memory + compute)");
@@ -85,7 +126,7 @@ int main(int argc, char *argv[]) {
 
   //@@ Insert code here
 
-  free(hostMaskData);
+  free(hostMaskData);                    //6. Free device memory
   wbImage_delete(outputImage);
   wbImage_delete(inputImage);
 

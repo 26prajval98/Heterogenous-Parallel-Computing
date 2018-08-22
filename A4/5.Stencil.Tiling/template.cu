60ac91fb424cdf1c22b0c45410302fec1cd4ccaa
26prajval98 <26prajval98@gmail.com>
2018-08-22 16:21:01 +0530

    Stencil Done

--- A4/5.Stencil.Tiling/template.cu
+++ A4/5.Stencil.Tiling/template.cu
@@ -1,161 +1,189 @@
-#include "wb.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
 
-#define THREAD_PER_DIM 3
-#define wbCheck(stmt)                                                      \
-	do                                                                     \
-	{                                                                      \
-		cudaError_t err = stmt;                                            \
-		if (err != cudaSuccess)                                            \
-		{                                                                  \
-			wbLog(ERROR, "Failed to run stmt ", #stmt);                    \
-			wbLog(ERROR, "Got CUDA error ...  ", cudaGetErrorString(err)); \
-			return -1;                                                     \
-		}                                                                  \
-	} while (0)
+__global__ void Stencil(int*,int*,int,int,int);
+bool CheckAnswer(int*,int*,int,int,int);
+void printMatrix(int* mat,int,int);
 
-__host__ __device__ int val(float *arr, int i, int j, int k, int width, int depth)
-{
-	return arr[(i * width + j) * depth + k];
+#define BW 8
+#define value(array, i, j, k) array[((i)*width + (j)) * depth + (k)]
+#define OutputArray(i, j, k) value(OutputArray, i, j, k)
+#define InputArray(i, j, k) value(InputArray, i, j, k)
+
+
+int main()
+{	
+	clock_t start_overhead,end_overhead;
+	start_overhead=clock();
+	
+    /* No. of rows and column in the image*/
+	int height=64;
+	int width=64;
+	int depth=64;
+	
+	/* Dynamically allocate memory to the input image, output image and the mask. Each matrix is represented as a 1D array*/
+	int* InputArray=new int[height*width*depth];
+	int* OutputArray=new int[height*width*depth];
+	
+	srand(time(NULL));
+	
+	/* Populate the input image and the mask with random numbers*/
+	for(int i=0;i<height;i++)
+	for(int j=0;j<width;j++)
+	for(int k=0;k<depth;k++)
+	InputArray(i,j,k)=rand()%4;
+
+  
+	int InputArraySize=height*width*depth*sizeof(int);
+	
+	
+	int* InputArray_d, *OutputArray_d;
+	
+	/* Allocate memory on the device*/
+	
+	cudaMalloc((void**)&InputArray_d,InputArraySize);
+	cudaMalloc((void**)&OutputArray_d,InputArraySize);
+	
+	/* Copy data from host to device*/
+	cudaMemcpy(InputArray_d,InputArray,InputArraySize,cudaMemcpyHostToDevice);
+
+	/* Invoke the Stencil kernel*/
+	dim3 dimBlock(BW,BW,BW);
+	dim3 dimGrid(ceil((float)width/BW),ceil((float)height/BW),ceil((float)depth/BW));
+	
+	clock_t start,end,total;
+	start=clock();
+	
+	Stencil<<<dimGrid,dimBlock>>>(InputArray_d,OutputArray_d,height,width,depth);
+	
+	end=clock();
+	total=(double)(end - start) / CLOCKS_PER_SEC;
+	printf("Time taken on device: %ld\n",total);
+	
+	/* Copy the convoluted image to host*/
+	cudaMemcpy(OutputArray,OutputArray_d,InputArraySize,cudaMemcpyDeviceToHost);
+	
+	end_overhead=clock();
+	
+	if(CheckAnswer(InputArray,OutputArray,height,width,depth))
+	printf("Solution is right!\n");
+    else
+	printf("Solution is wrong!\n");
+	
+	printf("Time spent on overhead calculations: %lf\n",(double)(end_overhead - start_overhead) / CLOCKS_PER_SEC);
+	
+/* 	printMatrix(InputArray,height,width);
+	printMatrix(OutputArray,height,width);
+	printMatrix(Mask,MaskWidth,MaskWidth); */
+	
+	
+	cudaFree(InputArray);
+	cudaFree(OutputArray);
 }
 
-__global__ void stencil(float *output, float *input, int width, int height, int depth)
+__global__ void Stencil(int* InputArray_d,int* OutputArray_d, int height, int width,int depth)
 {
-	__shared__ float cache[(THREAD_PER_DIM + 2) * (THREAD_PER_DIM + 2) * (THREAD_PER_DIM + 2)];
-	int gx = threadIdx.x + blockDim.x * blockIdx.x;
-	int gy = threadIdx.y + blockDim.y * blockIdx.y;
-	int gz = threadIdx.z + blockDim.z * blockIdx.z;
 
-	int lx = threadIdx.x + 1;
-	int ly = threadIdx.y + 1;
-	int lz = threadIdx.z + 1;
+#define InputArray_d(i, j, k) value(InputArray_d, i, j, k)
+#define OutputArray_d(i, j, k) value(OutputArray_d, i, j, k)
 
-	cache[val(lx, ly, lz, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)] = input[val(gx, gy, gz, width, depth)];
-
-	if (threadIdx.x == 0)
-	{
-		if (gx > 0)
-		{
-			cache[val(0, ly, lz, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)] =
-				input[val(gx - 1, gy, gz, width, depth)];
-		}
-		if (gx + THREAD_PER_DIM < height)
-		{
-			cache[val(THREAD_PER_DIM + 1, ly, lz, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)] =
-				input[val(gx + THREAD_PER_DIM, gy, gz, width, depth)];
-		}
-	}
-	if (threadIdx.y == 0)
-	{
-		if (gy > 0)
-		{
-			cache[val(lx, 0, lz, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)] =
-				input[val(gx, gy - 1, gz, width, depth)];
-		}
-		if (gy + THREAD_PER_DIM < width)
-		{
-			cache[val(lx, THREAD_PER_DIM + 1, lz, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)] =
-				input[val(gx, gy + THREAD_PER_DIM, gz, width, depth)];
-		}
-	}
-	if (threadIdx.z == 0)
-	{
-		if (gz > 0)
-		{
-			cache[val(lx, ly, 0, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)] =
-				input[val(gx, gy, gz - 1, width, depth)];
-		}
-		if (gz + THREAD_PER_DIM < depth)
-		{
-			cache[val(lx, ly, THREAD_PER_DIM + 1, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)] =
-				input[val(gx, gy, gz + THREAD_PER_DIM, width, depth)];
-		}
-	}
+  	/* Store the thread dimensions on registers*/
+	int bx=blockIdx.x,by=blockIdx.y,bz=blockIdx.z;
+	int tx=threadIdx.x,ty=threadIdx.y,tz=threadIdx.z;
+	
+	/* Declare a shared memory region for each block of threads*/
+	__shared__ int SharedMemBlock[BW][BW][BW];
+	
+	/* Find out the row and column for each thread*/
+	int XIdx=bx*blockDim.x+tx;
+	int YIdx=by*blockDim.y+ty;
+	int ZIdx=bz*blockDim.z+tz;
+	
+	SharedMemBlock[tx][ty][tz]=InputArray_d(XIdx,YIdx,ZIdx);
 	__syncthreads();
+	
+	if(XIdx>0 && XIdx<width-1 && YIdx>0 && YIdx<height-1 && ZIdx>0 && ZIdx<depth-1)
+	{		
 
-	if (gx > 0 && gy > 0 && gz > 0 && gx < height - 1 && gz < depth - 1 && gy < width - 1)
-	{
-		unsigned char cur = cache[val(lx, ly, lz, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)];
-		unsigned char down = cache[val(lx, ly + 1, lz, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)];
-		unsigned char up = cache[val(lx, ly - 1, lz, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)];
-		unsigned char left = cache[val(lx, ly, lz - 1, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)];
-		unsigned char right = cache[val(lx, ly, lz + 1, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)];
-		unsigned char front = cache[val(lx - 1, ly, lz, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)];
-		unsigned char back = cache[val(lx + 1, ly, lz, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)];
+		int Pvalue=0;
+		
+		if(ZIdx+1>=(bz+1)*BW)
+		Pvalue+=InputArray_d(XIdx,YIdx,ZIdx+1);
+		else
+		Pvalue+=SharedMemBlock[tx][ty][tz+1];
 
-		int res = right + left + up + down + front + back - 6 * cur;
-		res = Clamp(res, 0, 255);
+		if(ZIdx-1<bz*BW)
+		Pvalue+=InputArray_d(XIdx,YIdx,ZIdx-1);
+		else
+		Pvalue+=SharedMemBlock[tx][ty][tz-1];
 
-		output[val(gx, gy, gz, width, depth)] = res;
+		if(YIdx+1>=(by+1)*BW)
+		Pvalue+=InputArray_d(XIdx,YIdx+1,ZIdx);
+		else
+		Pvalue+=SharedMemBlock[tx][ty+1][tz];
+
+		if(YIdx-1<by*BW)
+		Pvalue+=InputArray_d(XIdx,YIdx-1,ZIdx);
+		else
+		Pvalue+=SharedMemBlock[tx][ty-1][tz];
+
+		if(XIdx+1>=(bx+1)*BW)
+		Pvalue+=InputArray_d(XIdx+1,YIdx,ZIdx);
+		else
+		Pvalue+=SharedMemBlock[tx+1][ty][tz];
+
+		if(XIdx-1<(bx)*BW)
+		Pvalue+=InputArray_d(XIdx-1,YIdx,ZIdx);
+		else
+		Pvalue+=SharedMemBlock[tx-1][ty][tz];
+
+		Pvalue-=6*InputArray_d(XIdx,YIdx,ZIdx);
+		OutputArray_d(XIdx,YIdx,ZIdx)=Pvalue;
 	}
+
+#undef OutputArray_d
+#undef InputArray_d
 }
 
-static void launch_stencil(float *deviceOutputData, float *deviceInputData, int width, int height, int depth)
+/* A function to verify correctness of solution*/
+bool CheckAnswer(int* InputArray, int* OutputArray,int height, int width,int depth)
 {
-	dim3 blockDim(THREAD_PER_DIM,THREAD_PER_DIM,THREAD_PER_DIM);
-	dim3 gridDim((height-1)/THREAD_PER_DIM+1,(width-1)/THREAD_PER_DIM+1,(depth-1)/THREAD_PER_DIM+1);
+	clock_t start,end;
+	start=clock();
+	for(int i=1;i<height-1;i++)
+	{
+		for(int j=1;j<width-1;j++)
+		{
+			for(int k=1;k<depth-1;k++)
+			{
+				int Pvalue=0;
+				Pvalue=InputArray(i,j,k+1)+InputArray(i,j,k-1)+
+				       InputArray(i,j+1,k)+InputArray(i,j-1,k)+
+					   InputArray(i+1,j,k)+InputArray(i-1,j,k)-
+					   6*InputArray(i,j,k);				  
+				       
+				if(Pvalue!=OutputArray(i,j,k))
+				return false;
+			}
 
-	stencil<<<gridDim, blockDim>>>(deviceOutputData, deviceInputData, width, height, depth);
+		}
+	}
+	end=clock();
+	printf("Time taken on host: %lf\n",(double)(end - start) / CLOCKS_PER_SEC);
+	return true;
+
 }
 
-int main(int argc, char *argv[])
+void printMatrix(int* mat,int nrow,int ncol)
 {
-	wbArg_t arg;
-	int width;
-	int height;
-	int depth;
-	char *inputFile;
-	wbImage_t input;
-	wbImage_t output;
-	float *hostInputData;
-	float *hostOutputData;
-	float *deviceInputData;
-	float *deviceOutputData;
-
-	arg = wbArg_read(argc, argv);
-
-	inputFile = wbArg_getInputFile(arg, 3);
-
-	input = wbImport(inputFile);
-
-	width = wbImage_getWidth(input);
-	height = wbImage_getHeight(input);
-	depth = wbImage_getChannels(input);
-
-	output = wbImage_new(width, height, depth);
-
-	hostInputData = wbImage_getData(input);
-	hostOutputData = wbImage_getData(output);
-
-	wbTime_start(GPU, "Doing GPU memory allocation");
-	cudaMalloc((void **)&deviceInputData,
-			   width * height * depth * sizeof(float));
-	cudaMalloc((void **)&deviceOutputData,
-			   width * height * depth * sizeof(float));
-	wbTime_stop(GPU, "Doing GPU memory allocation");
-
-	wbTime_start(Copy, "Copying data to the GPU");
-	cudaMemcpy(deviceInputData, hostInputData,
-			   width * height * depth * sizeof(float),
-			   cudaMemcpyHostToDevice);
-	wbTime_stop(Copy, "Copying data to the GPU");
-
-	wbTime_start(Compute, "Doing the computation on the GPU");
-	launch_stencil(deviceOutputData, deviceInputData, width, height, depth);
-	wbTime_stop(Compute, "Doing the computation on the GPU");
-
-	wbTime_start(Copy, "Copying data from the GPU");
-	cudaMemcpy(hostOutputData, deviceOutputData,
-			   width * height * depth * sizeof(float),
-			   cudaMemcpyDeviceToHost);
-	wbTime_stop(Copy, "Copying data from the GPU");
-
-	wbSolution(arg, output);
-
-	cudaFree(deviceInputData);
-	cudaFree(deviceOutputData);
-
-	wbImage_delete(output);
-	wbImage_delete(input);
-
-	return 0;
+	for(int i=0;i<nrow;i++)
+	{
+		for(int j=0;j<ncol;j++)
+		printf("%d ",mat[i*ncol+j]);
+	    
+		printf("\n");
+	}
+	printf("\n\n");
 }

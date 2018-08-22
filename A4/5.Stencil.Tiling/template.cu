07e995f7361d5a93d45e48d5030b684099e2e372
26prajval98 <26prajval98@gmail.com>
2018-08-22 14:40:29 +0530

    $

--- A4/5.Stencil.Tiling/template.cu
+++ A4/5.Stencil.Tiling/template.cu
@@ -1,83 +1,161 @@
 #include "wb.h"
 
-#define wbCheck(stmt)                                                     \
-  do {                                                                    \
-    cudaError_t err = stmt;                                               \
-    if (err != cudaSuccess) {                                             \
-      wbLog(ERROR, "Failed to run stmt ", #stmt);                         \
-      wbLog(ERROR, "Got CUDA error ...  ", cudaGetErrorString(err));      \
-      return -1;                                                          \
-    }                                                                     \
-  } while (0)
+#define THREAD_PER_DIM 3
+#define wbCheck(stmt)                                                      \
+	do                                                                     \
+	{                                                                      \
+		cudaError_t err = stmt;                                            \
+		if (err != cudaSuccess)                                            \
+		{                                                                  \
+			wbLog(ERROR, "Failed to run stmt ", #stmt);                    \
+			wbLog(ERROR, "Got CUDA error ...  ", cudaGetErrorString(err)); \
+			return -1;                                                     \
+		}                                                                  \
+	} while (0)
 
-__global__ void stencil(float *output, float *input, int width, int height,
-                        int depth) {
-  //@@ INSERT CODE HERE
+__host__ __device__ int val(float *arr, int i, int j, int k, int width, int depth)
+{
+	return arr[(i * width + j) * depth + k];
 }
 
-static void launch_stencil(float *deviceOutputData, float *deviceInputData,
-                           int width, int height, int depth) {
-  //@@ INSERT CODE HERE
+__global__ void stencil(float *output, float *input, int width, int height, int depth)
+{
+	__shared__ float cache[(THREAD_PER_DIM + 2) * (THREAD_PER_DIM + 2) * (THREAD_PER_DIM + 2)];
+	int gx = threadIdx.x + blockDim.x * blockIdx.x;
+	int gy = threadIdx.y + blockDim.y * blockIdx.y;
+	int gz = threadIdx.z + blockDim.z * blockIdx.z;
+
+	int lx = threadIdx.x + 1;
+	int ly = threadIdx.y + 1;
+	int lz = threadIdx.z + 1;
+
+	cache[val(lx, ly, lz, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)] = input[val(gx, gy, gz, width, depth)];
+
+	if (threadIdx.x == 0)
+	{
+		if (gx > 0)
+		{
+			cache[val(0, ly, lz, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)] =
+				input[val(gx - 1, gy, gz, width, depth)];
+		}
+		if (gx + THREAD_PER_DIM < height)
+		{
+			cache[val(THREAD_PER_DIM + 1, ly, lz, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)] =
+				input[val(gx + THREAD_PER_DIM, gy, gz, width, depth)];
+		}
+	}
+	if (threadIdx.y == 0)
+	{
+		if (gy > 0)
+		{
+			cache[val(lx, 0, lz, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)] =
+				input[val(gx, gy - 1, gz, width, depth)];
+		}
+		if (gy + THREAD_PER_DIM < width)
+		{
+			cache[val(lx, THREAD_PER_DIM + 1, lz, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)] =
+				input[val(gx, gy + THREAD_PER_DIM, gz, width, depth)];
+		}
+	}
+	if (threadIdx.z == 0)
+	{
+		if (gz > 0)
+		{
+			cache[val(lx, ly, 0, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)] =
+				input[val(gx, gy, gz - 1, width, depth)];
+		}
+		if (gz + THREAD_PER_DIM < depth)
+		{
+			cache[val(lx, ly, THREAD_PER_DIM + 1, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)] =
+				input[val(gx, gy, gz + THREAD_PER_DIM, width, depth)];
+		}
+	}
+	__syncthreads();
+
+	if (gx > 0 && gy > 0 && gz > 0 && gx < height - 1 && gz < depth - 1 && gy < width - 1)
+	{
+		unsigned char cur = cache[val(lx, ly, lz, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)];
+		unsigned char down = cache[val(lx, ly + 1, lz, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)];
+		unsigned char up = cache[val(lx, ly - 1, lz, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)];
+		unsigned char left = cache[val(lx, ly, lz - 1, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)];
+		unsigned char right = cache[val(lx, ly, lz + 1, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)];
+		unsigned char front = cache[val(lx - 1, ly, lz, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)];
+		unsigned char back = cache[val(lx + 1, ly, lz, THREAD_PER_DIM + 2, THREAD_PER_DIM + 2)];
+
+		int res = right + left + up + down + front + back - 6 * cur;
+		res = Clamp(res, 0, 255);
+
+		output[val(gx, gy, gz, width, depth)] = res;
+	}
 }
 
-int main(int argc, char *argv[]) {
-  wbArg_t arg;
-  int width;
-  int height;
-  int depth;
-  char *inputFile;
-  wbImage_t input;
-  wbImage_t output;
-  float *hostInputData;
-  float *hostOutputData;
-  float *deviceInputData;
-  float *deviceOutputData;
+static void launch_stencil(float *deviceOutputData, float *deviceInputData, int width, int height, int depth)
+{
+	dim3 blockDim(THREAD_PER_DIM,THREAD_PER_DIM,THREAD_PER_DIM);
+	dim3 gridDim((height-1)/THREAD_PER_DIM+1,(width-1)/THREAD_PER_DIM+1,(depth-1)/THREAD_PER_DIM+1);
 
-  arg = wbArg_read(argc, argv);
+	stencil<<<gridDim, blockDim>>>(deviceOutputData, deviceInputData, width, height, depth);
+}
 
-  inputFile = wbArg_getInputFile(arg, 0);
+int main(int argc, char *argv[])
+{
+	wbArg_t arg;
+	int width;
+	int height;
+	int depth;
+	char *inputFile;
+	wbImage_t input;
+	wbImage_t output;
+	float *hostInputData;
+	float *hostOutputData;
+	float *deviceInputData;
+	float *deviceOutputData;
 
-  input = wbImport(inputFile);
+	arg = wbArg_read(argc, argv);
 
-  width  = wbImage_getWidth(input);
-  height = wbImage_getHeight(input);
-  depth  = wbImage_getChannels(input);
+	inputFile = wbArg_getInputFile(arg, 3);
 
-  output = wbImage_new(width, height, depth);
+	input = wbImport(inputFile);
 
-  hostInputData  = wbImage_getData(input);
-  hostOutputData = wbImage_getData(output);
+	width = wbImage_getWidth(input);
+	height = wbImage_getHeight(input);
+	depth = wbImage_getChannels(input);
 
-  wbTime_start(GPU, "Doing GPU memory allocation");
-  cudaMalloc((void **)&deviceInputData,
-             width * height * depth * sizeof(float));
-  cudaMalloc((void **)&deviceOutputData,
-             width * height * depth * sizeof(float));
-  wbTime_stop(GPU, "Doing GPU memory allocation");
+	output = wbImage_new(width, height, depth);
 
-  wbTime_start(Copy, "Copying data to the GPU");
-  cudaMemcpy(deviceInputData, hostInputData,
-             width * height * depth * sizeof(float),
-             cudaMemcpyHostToDevice);
-  wbTime_stop(Copy, "Copying data to the GPU");
+	hostInputData = wbImage_getData(input);
+	hostOutputData = wbImage_getData(output);
 
-  wbTime_start(Compute, "Doing the computation on the GPU");
-  launch_stencil(deviceOutputData, deviceInputData, width, height, depth);
-  wbTime_stop(Compute, "Doing the computation on the GPU");
+	wbTime_start(GPU, "Doing GPU memory allocation");
+	cudaMalloc((void **)&deviceInputData,
+			   width * height * depth * sizeof(float));
+	cudaMalloc((void **)&deviceOutputData,
+			   width * height * depth * sizeof(float));
+	wbTime_stop(GPU, "Doing GPU memory allocation");
 
-  wbTime_start(Copy, "Copying data from the GPU");
-  cudaMemcpy(hostOutputData, deviceOutputData,
-             width * height * depth * sizeof(float),
-             cudaMemcpyDeviceToHost);
-  wbTime_stop(Copy, "Copying data from the GPU");
+	wbTime_start(Copy, "Copying data to the GPU");
+	cudaMemcpy(deviceInputData, hostInputData,
+			   width * height * depth * sizeof(float),
+			   cudaMemcpyHostToDevice);
+	wbTime_stop(Copy, "Copying data to the GPU");
 
-  wbSolution(arg, output);
+	wbTime_start(Compute, "Doing the computation on the GPU");
+	launch_stencil(deviceOutputData, deviceInputData, width, height, depth);
+	wbTime_stop(Compute, "Doing the computation on the GPU");
 
-  cudaFree(deviceInputData);
-  cudaFree(deviceOutputData);
+	wbTime_start(Copy, "Copying data from the GPU");
+	cudaMemcpy(hostOutputData, deviceOutputData,
+			   width * height * depth * sizeof(float),
+			   cudaMemcpyDeviceToHost);
+	wbTime_stop(Copy, "Copying data from the GPU");
 
-  wbImage_delete(output);
-  wbImage_delete(input);
+	wbSolution(arg, output);
 
-  return 0;
+	cudaFree(deviceInputData);
+	cudaFree(deviceOutputData);
+
+	wbImage_delete(output);
+	wbImage_delete(input);
+
+	return 0;
 }

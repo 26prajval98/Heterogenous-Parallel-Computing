2db4c594d67d6195c0ad7ec03ed1903802cc4b5b
SUMUKHA-PK <sumukhapk46@gmail.com>
2018-09-20 20:52:23 +0530

    BLSH

--- A5 & A6/A5/Q1.c
+++ A5 & A6/A5/Q1.c
@@ -4,15 +4,17 @@ void main()
 {
     //Serial code
     double start1 = omp_get_wtime();
-    printHello_Serial();
+    printf("Hello World!\n");
     double end1 = omp_get_wtime();
     printf("Time taken by serial code: %f\n",end1-start1);
 
     //Parallel code
+    start1 = omp_get_wtime();
     #pragma omp parallel
     {
         int ID = omp_get_thread_num();
         printf("Hello World\n");
     }
-    
+    end1 = omp_get_wtime();
+    printf("Time taken by parallel code: %f\n",end1-start1);
 }
--- A5 & A6/A5/Q5.c
+++ A5 & A6/A5/Q5.c
@@ -1,46 +1,65 @@
 #include "omp.h"
-#include<stdio.h>
-#include<time.h>
+#include <stdio.h>
+#include <time.h>
+
+#define MAX_THREADS 4
+
 static long num_steps = 100000;
+
 double step;
 double pi_s();
+
+void parallel()
+{
+    int i, j;
+    double pi, full_sum = 0.0;
+    double start_time, run_time;
+    double sum[MAX_THREADS];
+
+    step = 1.0 / (double)num_steps;
+
+    for (j = 1; j <= MAX_THREADS; j++)
+    {
+        omp_set_num_threads(j);
+        full_sum = 0.0;
+        start_time = omp_get_wtime();
+#pragma omp parallel private(i)
+        {
+            int id = omp_get_thread_num();
+            int numthreads = omp_get_num_threads();
+            double x;
+
+            double partial_sum = 0;
+
+#pragma omp single
+            printf(" num_threads = %d", numthreads);
+
+            for (i = id; i < num_steps; i += numthreads)
+            {
+                x = (i + 0.5) * step;
+                partial_sum += +4.0 / (1.0 + x * x);
+            }
+#pragma omp critical
+            full_sum += partial_sum;
+        }
+
+        pi = step * full_sum;
+        run_time = omp_get_wtime() - start_time;
+        printf("\n pi is %f in %f seconds %d threads \n ", pi, run_time, j);
+    }
+}
+
 void main()
 {
     //Calculate serial code's time and output
     double start = omp_get_wtime();
-    double pi_serial  = pi_s();
+    double pi_serial = pi_s();
     double end = omp_get_wtime();
-    printf("Time taken by serial code is %f\n",end-start);
- 
-    printf("Serial output value: %f\n",pi_serial);
+    printf("Time taken by serial code is %f\n", end - start);
 
-    //Calculate parallel code's output and answer
-    int i,nthrds;
-    double x, pi, sum = 0.0;
-    step = 1.0/(double)num_steps;
-    omp_set_num_threads(10000);
-    start = omp_get_wtime();
-    #pragma omp parallel
-    {
-        float partial_sum;
-        int i, id;
-        id = omp_get_thread_num();
-        nthrds = omp_get_num_threads();
-        step = 1.0/(double)num_steps;
-        for(i=id;i<num_steps;i=i+nthrds)
-        {
-            double x = (i+0.5)*step;
-            partial_sum= 4.0/(1.0+x*x);
-            #pragma omp atomic                    //To ensure atomicity while adding
-                sum = sum + partial_sum;
-        }
-    }
-    end = omp_get_wtime();
+    printf("Serial output value: %f\n", pi_serial);
 
-    printf("Time taken by parallel code: %f\n",end-start);
-    printf("Total threads = %d\n",nthrds);
-    pi = step * sum;
-    printf("Parallel Value: %f\n",pi);
+    parallel();
 }
 
 //Serial code
@@ -48,11 +67,11 @@ double pi_s()
 {
     int i;
     double x, pi, sum = 0.0;
-    step = 1.0/(double)num_steps;
-    for (i=0; i<num_steps; i++)
+    step = 1.0 / (double)num_steps;
+    for (i = 0; i < num_steps; i++)
     {
-        x = (i+0.5)*step;
-        sum = sum + 4.0/(1.0+x*x);
+        x = (i + 0.5) * step;
+        sum = sum + 4.0 / (1.0 + x * x);
     }
     pi = step * sum;
     return pi;

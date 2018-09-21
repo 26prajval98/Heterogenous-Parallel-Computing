1547025420fe3c58189b98546cd292085e8c1952
SUMUKHA-PK <sumukhapk46@gmail.com>
2018-09-21 09:44:44 +0530

    Changes to correct

--- A5/Q6/Q6.c
+++ A5/Q6/Q6.c
@@ -13,29 +13,43 @@ double pi_s();
 
 void parallel()
 {
-	int  j;
-	double sum = 0.0;
-	double start_time, run_time;
+    int i, j;
+    double pi, full_sum = 0.0;
+    double start_time, run_time;
+    double sum[MAX_THREADS];
 
-	step = 1.0 / (double)num_steps;
+    step = 1.0 / (double)num_steps;
 
-	for (j = 1; j <= MAX_THREADS; j++)
-	{
-		omp_set_num_threads(j);
-		sum = 0.0;
-		double x = 0;
-		start_time = omp_get_wtime();
-#pragma omp parallel reduction(+: sum)
-		{
-#pragma omp for
-			for (int i = 0; i < num_steps; i++) {
-				x = (i - 0.5) * step;
-				sum = sum + 4.0 / (1.0 + x * x);
-			}
-		}
-		run_time = omp_get_wtime() - start_time;
-		printf("\n pi is %f in %f seconds %d threads \n ", sum/num_steps, run_time, j);
-	}
+    for (j = 1; j <= MAX_THREADS; j++)
+    {
+        omp_set_num_threads(j);
+        full_sum = 0.0;
+        start_time = omp_get_wtime();
+        #pragma omp parallel private(i)
+        {
+            int id = omp_get_thread_num();
+            int numthreads = omp_get_num_threads();
+            double x;
+
+            double partial_sum = 0;
+
+        #pragma omp single
+            printf(" num_threads = %d", numthreads);
+
+        #pragma omp for
+            for (i = id; i < num_steps; i += numthreads)
+            {
+                x = (i + 0.5) * step;
+                partial_sum += +4.0 / (1.0 + x * x);
+            }
+        #pragma omp critical
+            full_sum += partial_sum;
+        }
+
+        pi = step * full_sum;
+        run_time = omp_get_wtime() - start_time;
+        printf("\n pi is %f in %f seconds %d threads \n ", pi, run_time, j);
+    }
 }
 
 void main()
